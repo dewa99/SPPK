fb3e21d978b060b906472ed90c7b28d9277ce4fa
JohnKontolatos <dodeardi@gmail.com>
2019-06-26 21:36:00 +0700

    nope

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,6 +1,6 @@
 <title>Lab Pemrograman Fasilkom Unej</title>
 <link href="{{secure_asset('css/bootstrap.css')}}" rel='stylesheet' type='text/css' />
-<link href="{{sercure_asset('css/style.css')}}" rel='stylesheet' type='text/css' />
+<link href="{{secure_asset('css/style.css')}}" rel='stylesheet' type='text/css' />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
