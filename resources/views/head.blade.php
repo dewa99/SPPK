7ab64fa15222e2b692a2f79b6fb62275f0dfd759
JohnKontolatos <dodeardi@gmail.com>
2019-06-26 21:37:32 +0700

    nope

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -4,7 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="{{asset('css/fwslider.css')}}" media="all">
+<link rel="stylesheet" href="{{secure_asset('css/fwslider.css')}}" media="all">
 <script src="{{secure_asset('js/jquery-ui.min.js')}}"></script>
 <script src="{{secure_asset('js/fwslider.js')}}"></script>
 <script src="{{secure_asset('js/jquery.min.js')}}"></script>
