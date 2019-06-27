c369916061c1c2e25bd6ffcb3a87594eca9a2783
MEMED <dokargaul@gmail.com>
2019-06-27 08:19:49 +0700

    fixing

--- public/admin/application/views/login.php
+++ public/admin/application/views/login.php
@@ -28,7 +28,7 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="../../index2.html"><b>Hallo Admin <i class="fa fa-hand-stop-o" aria-hidden="true"></i></a>
+            <b>Hallo Admin <i class="fa fa-hand-stop-o" aria-hidden="true"></i>
         </div>
         <!-- /.login-logo -->
         <div class="login-box-body">
